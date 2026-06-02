73e5588bc1b3059db17903a474e69432071b61f8
JaiW24 <jwhite66@smartweb.augustatech.edu>
2026-06-01 20:45:05 -0400

    Update chaos4.py

--- chaos4.py
+++ chaos4.py
@@ -1,1 +1,10 @@
+def main():
+    print("This program illustrates a chaotic function")
 
+    x = float(input("Enter a number between 0 and 1: "))
+
+    for i in range(30):
+        x = 3.5 * x * (1 - x)
+        print(x)
+
+main()
