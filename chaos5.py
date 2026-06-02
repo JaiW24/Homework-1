3fe682eeee49ea5999f36ceeac1a55bc4cc956c7
JaiW24 <jwhite66@smartweb.augustatech.edu>
2026-06-01 21:43:21 -0400

    Update chaos5.py

--- chaos5.py
+++ chaos5.py
@@ -1,1 +1,11 @@
+def main():
+    print("This program illustrates a chaotic function")
 
+    x = float(input("Enter a number between 0 and 1: "))
+    k = float(input("Enter a value for k: "))
+
+    for i in range(30):
+        x = k * x * (1 - x)
+        print(x)
+
+main()
